5ca80f6e4556e5cf8ce8f8342419369ecbd9d918
Mateus <Mateusduduarte18@gmail.com>
2024-09-05 16:17:43 -0300

    Titulo pronto

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import "./Title.css" 
+import logo from '../assets/WolLogo1.webp';
+
+const Title = () => {
+    return (
+        <div className="title">
+            <img className="logo_img" src={logo} alt="logo" />
+            <h1>Owl Focus</h1>
+        </div>
+    )
+}
+
+export default Title
